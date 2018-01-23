68ebcbfd1784b7834f4b3ac0eb839e78377f14a1
Daniel Chung <danielchung@lawn-128-61-23-117.lawn.gatech.edu>
2018-01-23 13:12:42 -0500

    change comment layout, added exercise

--- rec2/SwitchStatements.java
+++ rec2/SwitchStatements.java
@@ -1,11 +1,12 @@
 public class SwitchStatements {
 	public static void main(String[] args) {
-		// PROBLEM: How would you write a converter from numerical to letter grades?
+	
+	// PROBLEM: How would you write a converter from numerical to letter grades?
 
 		double grade = 99.3;
 		char letGrade = '\0'; // why this and not ''?
 
-		// review of multi-way if else
+	// REVIEW: multi-way if else
 		if (grade >= 90.0) {
 			letGrade = 'A';
 		} else if (grade >= 80.0) {
@@ -20,7 +21,7 @@ public class SwitchStatements {
 
 		System.out.printf("A %.2f is a(n) %c\n", grade, letGrade);
 
-		// using a switch statement to print messages for each grade
+	// CONCEPT: switch statement (to print messages for each grade)
 		switch (letGrade) {
 			case 'A':
 				System.out.println("Well done! We'd love if every one of you got one of these");
@@ -40,7 +41,29 @@ public class SwitchStatements {
 				System.out.println("Invalid letter grade");
 		}
 
-		// QUESTION: what happens if you get an A?
+	// QUESTION: what will be printed if you get a B? What about an A?
 
+		System.out.println();
+
+	// EXERCISE: turn the following multi-way if-else statement into a switch statement
+		String taName = "Viren";
+		System.out.printf("TA: %s\n", taName);
+
+		// if-else
+		if (taName.equals("Harsh")) {
+			System.out.println("I'm going to grad school after graduation!");
+		} else if (taName.equals("Viren") || taName.equals("Daniel")) {
+			if (taName.equals("Viren")) {
+				System.out.println("I took this class last Fall");
+			}
+			System.out.println("My major is CS");
+		} else {
+			System.out.println("I'm not a TA for this section :/");
+		}
+
+		// your code starts here
+
+
+		// your code ends here
 	}
 }
